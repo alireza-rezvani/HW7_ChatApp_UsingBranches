753926045bdef702e6edacf5e54c56f755125213
arfjava <arrfjava@gmail.com>
2019-12-18 11:34:53 +0330

    classes ClientDemo & ClientDemo2 created to test chatting in same time.

---
+++ src/ir/maktab32/java/projects/homework7/chatapp/client/ClientDemo.java
@@ -0,0 +1,7 @@
+package ir.maktab32.java.projects.homework7.chatapp.client;
+
+public class ClientDemo {
+    public static void main(String[] args) {
+        new Client().execute();
+    }
+}
---
+++ src/ir/maktab32/java/projects/homework7/chatapp/client/ClientDemo2.java
@@ -0,0 +1,7 @@
+package ir.maktab32.java.projects.homework7.chatapp.client;
+
+public class ClientDemo2 {
+    public static void main(String[] args) {
+        new Client().execute();
+    }
+}
