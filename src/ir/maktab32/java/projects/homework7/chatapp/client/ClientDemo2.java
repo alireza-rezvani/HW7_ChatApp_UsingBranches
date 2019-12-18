799146a9c6165f15d5cff183d147f0794f4014b4
arfjava <arrfjava@gmail.com>
2019-12-18 12:07:32 +0330

    phase3 completed.

--- src/ir/maktab32/java/projects/homework7/chatapp/client/ClientDemo2.java
+++ src/ir/maktab32/java/projects/homework7/chatapp/client/ClientDemo2.java
@@ -2,6 +2,6 @@ package ir.maktab32.java.projects.homework7.chatapp.client;
 
 public class ClientDemo2 {
     public static void main(String[] args) {
-        new Client().execute();
+        Login.execute();
     }
 }
--- src/ir/maktab32/java/projects/homework7/chatapp/client/Login.java
+++ src/ir/maktab32/java/projects/homework7/chatapp/client/Login.java
@@ -6,23 +6,23 @@ import ir.maktab32.java.projects.homework7.chatapp.model.User;
 import java.util.Scanner;
 
 public class Login {
-    private Scanner scanner = new Scanner(System.in);
-    private String username;
-    private String password;
-    private String serverIp;
-    private String serverPort;
+    private static Scanner scanner = new Scanner(System.in);
+    private static String username;
+    private static String password;
+    private static String serverIp;
+    private static String serverPort;
 
-    public void execute(){
+    public static void execute(){
         boolean loginAsNewUser = true;
 
         while (loginAsNewUser) {
             boolean isUserValid = false;
             boolean isServerValid = false;
-            System.out.println("Login");
+            System.out.println("\u2705 Login");
             while (!isUserValid) {
-                System.out.print("Username: ");
+                System.out.print("\t\u29bf Username: ");
                 username = scanner.next();
-                System.out.print("Password: ");
+                System.out.print("\t\u29bf Password: ");
                 password = scanner.next();
 
                 for (User i : DataSource.users) {
@@ -30,25 +30,25 @@ public class Login {
                         isUserValid = true;
                 }
                 if (!isUserValid)
-                    System.out.println("Invalid Username or Password!");
+                    System.out.println("\t\t\u26a0 Invalid Username or Password!");
             }
 
             while (!isServerValid) {
-                System.out.print("Server IP: ");
+                System.out.print("\t\u29bf Server IP: ");
                 serverIp = scanner.next();
-                System.out.print("Server Port: ");
+                System.out.print("\t\u29bf Server Port: ");
                 serverPort = scanner.next();
 
                 if ((serverIp.equals("localhost") || serverIp.equals("127.0.0.1")) && serverPort.equals("8080"))
                     isServerValid = true;
                 else
-                    System.out.println("Invalid IP or Port!");
+                    System.out.println("\t\t\u26a0 Invalid IP or Port!");
             }
 
-            System.out.println("Login Successful!");
+            System.out.println("\t\t\u2705 Login Successful!");
             new Client().execute();
 
-            System.out.println("Do You Want to Login as Another Client? y/other key");
+            System.out.println("\t\u29bf Do You Want to Login as Another Client?\t(y: Yes | other keys: No)");
             String answer = scanner.next();
             if (!answer.equals("y"))
                 loginAsNewUser = false;
