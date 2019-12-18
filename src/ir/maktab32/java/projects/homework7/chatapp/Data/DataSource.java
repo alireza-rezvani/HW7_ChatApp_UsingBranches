00c24a23fcb767c6a84e7dea0c558c5749005e09
arfjava <arrfjava@gmail.com>
2019-12-18 11:50:13 +0330

    DataSource completed.

---
+++ src/ir/maktab32/java/projects/homework7/chatapp/Data/DataSource.java
@@ -0,0 +1,13 @@
+package ir.maktab32.java.projects.homework7.chatapp.Data;
+
+import ir.maktab32.java.projects.homework7.chatapp.model.User;
+
+public class DataSource {
+    public static User[] users = {
+            new User("ali","1"),
+            new User("alireza","1"),
+            new User("reza","1"),
+            new User("mohammad","1"),
+            new User("user1","1")
+    };
+}
