b79cc3b21f3c07c9465405487c67c395d399916c
arfjava <arrfjava@gmail.com>
2019-12-18 10:48:05 +0330

    class Server edited to be running always.

--- src/ir/maktab32/java/projects/homework7/chatapp/Server.java
+++ src/ir/maktab32/java/projects/homework7/chatapp/Server.java
@@ -20,35 +20,37 @@ public class Server {
             server = new ServerSocket(port);
             System.out.println("\u2705 Server Created Successfully!");
 
-            System.out.println("\u29bf Server is Waiting for Clients...");
-            Socket client = server.accept();
-            System.out.println("\u2705 A Client Connected to Server Successfully!\n");
+            while (true) {
+                System.out.println("\n\u29bf Server is Waiting for Clients...");
+                Socket client = server.accept();
+                System.out.println("\u2705 A Client Connected to Server Successfully!\n");
 
-            reader = new DataInputStream(client.getInputStream());
-            writer = new PrintWriter(client.getOutputStream(), true);
+                reader = new DataInputStream(client.getInputStream());
+                writer = new PrintWriter(client.getOutputStream(), true);
 
-            String serverMsg;
-            String clientMsg;
-            String clientName;
-            serverMsg = "Hello Client! What is Your Name?";
-            System.out.println("Server: " + serverMsg);
-            writer.println(serverMsg);
-            clientName =  reader.readLine();
-            System.out.println("Client: " + clientName);
-            serverMsg = "Welcome " + clientName + "!";
-            System.out.println("Server: " + serverMsg);
-            writer.println(serverMsg);
-
-            while (true){
-                clientMsg = reader.readLine();
-                System.out.println(clientName + ": " + clientMsg);
-
-                if (clientMsg.equals("exit"))
-                    break;
-
-                System.out.print("Server: ");
-                serverMsg = scanner.nextLine();
+                String serverMsg;
+                String clientMsg;
+                String clientName;
+                serverMsg = "Hello Client! What is Your Name?";
+                System.out.println("Server: " + serverMsg);
                 writer.println(serverMsg);
+                clientName = reader.readLine();
+                System.out.println("Client: " + clientName);
+                serverMsg = "Welcome " + clientName + "!";
+                System.out.println("Server: " + serverMsg);
+                writer.println(serverMsg);
+
+                while (true) {
+                    clientMsg = reader.readLine();
+                    System.out.println(clientName + ": " + clientMsg);
+
+                    if (clientMsg.equals("exit"))
+                        break;
+
+                    System.out.print("Server: ");
+                    serverMsg = scanner.nextLine();
+                    writer.println(serverMsg);
+                }
             }
 
         } catch (IOException e) {
